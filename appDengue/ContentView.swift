f8140c22d02bc85a64121a8d901d7d7d934fbdda
Fernando Caletti <fe.caletti@gmail.com>
2022-07-25 10:23:21 -0300

    initial commit

---
+++ appDengue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  appDengue
+//
+//  Created by aluno13 on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ appDengue/appDengueApp.swift
@@ -0,0 +1,17 @@
+//
+//  appDengueApp.swift
+//  appDengue
+//
+//  Created by aluno13 on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct appDengueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
