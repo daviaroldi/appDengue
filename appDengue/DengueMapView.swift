9bac5f31dd7a2be9aba5e7ae6e121b5f6a9b5d6a
aluno12 <aluno12@MacBook-Pro-de-eldorado.local>
2022-07-27 11:30:31 -0300

    merge

--- appDengue/DengueMapView.swift
+++ appDengue/DengueMapView.swift
@@ -9,6 +9,23 @@ import SwiftUI
 import MapKit
 import CoreLocation
 
+//class Model: ObservableObject {
+//    @Published var selection: Int? {
+//        willSet {
+//            if let nv = newValue {
+//                selected = nv
+//                willChangeSelection?(selected)
+//            }
+//        }
+//    }
+//    var selected: Int = 0
+//    let willChangeSelection: ((Int) -> Void)?
+//    init( onSelection: ((Int)->Void)? ) {
+//        willChangeSelection = onSelection
+//        selection = 1
+//    }
+//}
+
 struct DengueMapView: View {
     @State var tracking : MapUserTrackingMode = .follow
 
@@ -17,6 +34,12 @@ struct DengueMapView: View {
     @StateObject var managerDelegate = locationDelegate()
     
     @Binding var focusPoints : [Pin]
+    
+//    @ObservedObject var model = Model { i in
+//        if managerDelegate.location == nil {
+//
+//        }
+//    }
 
     var body: some View {
         VStack{
@@ -32,17 +55,36 @@ struct DengueMapView: View {
 
                 }.edgesIgnoringSafeArea(.all)
                 
-                NavigationLink(
-                    destination: FocusRegistrationView(focusPoints: self._focusPoints)
-                        .environmentObject(managerDelegate),
-                    label: {
-                        Text("Registrar Foco")
-                    })
+//                NavigationLink(
+//                    destination: FocusRegistrationView(focusPoints: self._focusPoints)
+//                                    .environmentObject(managerDelegate),
+//                    tag: 1,
+//                    selection: $model.selection) {
+//                    Text("Registrar Foco")
+//                }
+//                let _test = managerDelegate.location == nil
+//                NavigationLink(
+//                    destination: defineDestination(),
+//                    isActive: ,
+//                    label: {
+//                        
+//                    }
+//                )
 
             }
 
         }.onAppear{
             manager.delegate = managerDelegate
+        }
+    }
+    
+    @ViewBuilder
+    func defineDestination() -> some View {
+        if managerDelegate.location == nil {
+            EmptyView()
+        } else {
+            FocusRegistrationView(focusPoints: self._focusPoints)
+                        .environmentObject(managerDelegate)
         }
     }
 }
