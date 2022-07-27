6a7741c4f67e35cbf3ed1219dd1e3f28ecb4a7cd
Diego Skieresz <diegoskieresz@gmail.com>
2022-07-27 10:31:39 -0300

    wip inspect view2

--- appDengue/OversightGuideView.swift
+++ appDengue/OversightGuideView.swift
@@ -8,129 +8,47 @@
 import SwiftUI
 
 struct OversightGuideView: View {
+    var oversights: [OversightModel] = [
+        OversightModel(imageName: "pneu", name: "Pneus Velhos", description: "Entregue seus pneus velhos ao serviço de limpeza urbana ou guarde-os sem água em local coberto"),
+        OversightModel(imageName: "vaso", name: "Plantas e Jardins", description: "Evite ter bromélias e outras plantas que acumulam água, ou retire semanalmente a água das folhas. Mantenha os pratinhos dos vasos de planta limpos e encha de areia até a borda."),
+        OversightModel(imageName: "garrafa", name: "Garrafas e latas", description: "Inspecionar todas as áreas da propriedade e retirar dos locais descobertos qualquer objeto descartado que possa acumular água."),
+        OversightModel(imageName: "poco", name: "Caixas de água, cisterna e poços", description: "Mantenha-as bem fechadas. Poços lacrar com concreto, caso haja um cano este deve ser vedado com uma tela."),
+        OversightModel(imageName: "grama", name: "Áreas de propriedade", description: "Cacos de vidro em muros, cavidades em cercas de pedra, muros, pedras, árvores e outros devem ser tampadas com barro ou cimento. Evitar deixar baldes, carrinhos de mão e outros utensílios que acumulam água ao relento. Inspecionar todas as áreas da propriedade, inclusive reservas legais, e retirar dos locais descobertos."),
+        OversightModel(imageName: "calha", name: "Calhas", description: "Verificar se não estão entupidas. Limpe-as (Principalmente no período chuvoso). nivele-as, remova folhas, galhos e tudo que possa impedir a água de correr pelas calhas."),
+        OversightModel(imageName: "laje", name: "Laje", description: "Não deixe água acumulada sobre a laje. mantenha-as sempre secas, caso tenha um desvio complete com terra."),
+        OversightModel(imageName: "piscina", name: "Piscina", description: "Mantenha a piscina sempre limpa. Use cloro para tratar a água e o filtro periodicamente. Caso estejam em desuso. Limpar uma vez por semana, Devem estar cobertas se não estiverem sendo usadas. Se estiverem vazias, colocar sal no ponto mais raso.")
+    ]
+    
     var body: some View {
         ScrollView(.vertical) {
             VStack (alignment: .leading){
                 
                 Divider()
                 
-                HStack (spacing: 16) {
-                    Image("pneu")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-        
-                    VStack(alignment: .leading) {
-                        Text("Pneus Velhos")
-                            .foregroundColor(.orange)
-                        Text("Entregue seus pneus velhos ao serviço de limpeza urbana ou guarde-os sem água em local coberto")
-                            .foregroundColor(.gray)
+                ForEach(oversights) { oversight in
+                    HStack (spacing: 16) {
+                        Image(oversight.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 90, height: 90)
+                            .background(
+                                Color.white
+                            )
+                            .clipShape(
+                                RoundedRectangle(cornerRadius: 20)
+                            )
+            
+                        VStack(alignment: .leading) {
+                            Text(oversight.name)
+                                .foregroundColor(.orange)
+                            Text(oversight.description)
+                                .foregroundColor(.gray)
+                        }
                     }
+                    Divider()
                 }
                 
-                Divider()
-                
-                HStack (spacing: 16) {
-                    Image("vaso")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-    
-                    VStack(alignment: .leading) {
-                        Text("Plantas e Jardins")
-                            .foregroundColor(.orange)
-                        Text("Evite ter bromélias e outras plantas que acumulam água, ou retire semanalmente a água das folhas. Mantenha os pratinhos dos vasos de planta limpos e encha de areia até a borda.")
-                            .foregroundColor(.gray)
-                    }
-                }
-                HStack (spacing: 16) {
-                    Image("garrafa")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-                   
-                    VStack(alignment: .leading) {
-                        Text("Garrafas e latas")
-                            .foregroundColor(.orange)
-                        Text("Inspecionar todas as áreas da propriedade e retirar dos locais descobertos qualquer objeto descartado que possa acumular água.")
-                            .foregroundColor(.gray)
-                    }
-                }
-                HStack (spacing: 16) {
-                    Image("poco")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-                        
-                    VStack(alignment: .leading) {
-                        Text("Caixas de água, cisterna e poços")
-                            .foregroundColor(.orange)
-                        Text("Mantenha-as bem fechadas. Poços lacrar com concreto, caso haja um cano este deve ser vedado com uma tela.")
-                            .foregroundColor(.gray)
-                    }
-                }
-                HStack (spacing: 16) {
-                    Image("grama")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-               
-                    VStack(alignment: .leading) {
-                        Text("Áreas de propriedade")
-                            .foregroundColor(.orange)
-                        Text("Cacos de vidro em muros, cavidades em cercas de pedra, muros, pedras, árvores e outros devem ser tampadas com barro ou cimento. Evitar deixar baldes, carrinhos de mão e outros utensílios que acumulam água ao relento. Inspecionar todas as áreas da propriedade, inclusive reservas legais, e retirar dos locais descobertos.")
-                            .foregroundColor(.gray)
-                    }
-                }
-                HStack (spacing: 16) {
-                    Image("calha")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 90, height: 90)
-                        .background(
-                            Color.white
-                        )
-                        .clipShape(
-                            RoundedRectangle(cornerRadius: 20)
-                        )
-               
-                    VStack(alignment: .leading) {
-                        Text("Calhas")
-                            .foregroundColor(.orange)
-                        Text("Verificar se não estão entupidas. Limpe-as (Principalmente no período chuvoso). nivele-as, remova folhas, galhos e tudo que possa impedir a água de correr pelas calhas.")
-                            .foregroundColor(.gray)
-                    }
-                }
+
         }
             .padding(8)
 
---
+++ appDengue/OversigthModel.swift
@@ -0,0 +1,18 @@
+//
+//  oversigthModelView.swift
+//  appDengue
+//
+//  Created by aluno6 on 27/07/22.
+//
+
+import Foundation
+
+struct OversightModel: Identifiable{
+    var id: String {
+        name
+    }
+    var imageName: String
+    var name: String
+    var description: String
+    
+}
